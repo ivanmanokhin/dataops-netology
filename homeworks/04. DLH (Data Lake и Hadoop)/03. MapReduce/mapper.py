549f794cdd9da3f8ba4083851f571aba2a63d63b
ivanmanokhin <manokhin@zoho.com>
2023-01-04 13:35:54 +0300

    [upd] homework 4.3 (MapReduce)

--- homeworks/04. DLH (Data Lake и Hadoop)/03. MapReduce/mapper.py
+++ homeworks/04. DLH (Data Lake и Hadoop)/03. MapReduce/mapper.py
@@ -6,4 +6,4 @@ next(stream) # пропускаем первую строку
 for line in stream:
     words = line.strip().split(',')[0].split()
     for word in words:
-        print(f'{ word },1')
+        print(f'{word},1')
